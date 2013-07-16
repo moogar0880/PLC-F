70c17735238848a17d7c56156cdeb38da2dee4c5
moogar0880 <moogar@comcast.net>
2013-07-16 19:38:59 -0400

    Wrote additional tests to try and break my code.
    Wrote additional tests to try and break my code and was luckily
    unsuccessful. Program passes all original and new tests.

--- HW5/main.py
+++ HW5/main.py
@@ -33,22 +33,21 @@ Looks at the words in the string, s, and returns a list of tuples in the
 form (word,count) sorted by highest count to lowest
 '''
 def wordCounts(s):
-    words = {}
-    toRet = []
-    for word in s.split():
-        word = word.lower()
-        if word[0] in string.punctuation:
-            word = word[1:]
-        if word[len(word)-1] in string.punctuation:
-            word = word[:-1]
-        if word in words.keys():
-            words[word] = words[word] + 1
-        else:
-            words[word] = 1
-    for word in words.keys():
-        toRet.append((word,words[word]))
-
-    return sorted(toRet, key=lambda wordcount: wordcount[1], reverse=True)
+  words = {}
+  toRet = []
+  for word in s.split():
+    word = word.lower()
+    if word[0] in string.punctuation:
+      word = word[1:]
+    if word[len(word)-1] in string.punctuation:
+      word = word[:-1]
+    if word in words.keys():
+      words[word] = words[word] + 1
+    else:
+      words[word] = 1
+  for word in words.keys():
+    toRet.append((word,words[word]))
+  return sorted(toRet, key=lambda wordcount: wordcount[1], reverse=True)
 
 '''
 Returns the nth prime number, starting with nthPrime(0) returning 1. The
@@ -176,100 +175,3 @@ class USDollar(object):
 
     def __str__(self):
         return self.__repr__()
-
-class Tester(object):
-    """docstring for Tester"""
-    def __init__(self):
-        super(Tester, self).__init__()
-        self.score = self.total = 0
-
-    def printResults(self,results,name):
-        for r in results:
-            self.total += 1
-            if r:
-                print "Pass {}, test {}".format(name, results.index(r))
-                self.score += 1
-            else:
-                print "Fail {}, test {}".format(name, results.index(r))
-
-    def filterTests(self):
-        results = []
-        def check(v):
-            return v % 2 == 0
-        #filter(check,range(20)
-        results.append(filter(check,range(20)) == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
-        self.printResults(results,'filterTests')
-
-    def matchBracketTests(self):
-        results = []
-        #matchBrackets("{{}")
-        results.append(matchBrackets("{{}") == False)
-        #matchBrackets("({)}")
-        results.append(matchBrackets("({)}") == False)
-        #matchBrackets("({})")
-        results.append(matchBrackets("({})") == True)
-        self.printResults(results,'matchBracketTests')
-
-    def wordCountsTests(self):
-        results = []
-        #wordCounts("a a a a liggity giggity lizzist")
-        results.append(wordCounts("a a a a liggity giggity lizzist") == [('a', 4), ('giggity', 1), ('lizzist', 1), ('liggity', 1)])
-        self.printResults(results,'wordCountsTests')
-
-    def nthPrimeTests(self):
-        results = []
-        results.append(nthPrime(10) == 29)
-        print nthPrime(10)
-        #print nthPrime.primes
-        self.printResults(results,'nthPrimeTests')
-
-    def uSDTests(self):
-        results = []
-        d1 = USDollar()
-        d1.val = 2.001
-        d2 = USDollar(100.0)
-        #Check innate rounding, d1.val == 2.0
-        results.append(d1.val == 2.0)
-        #Check __repr__
-        print "\t", d1.__repr__() == '$2.00'
-        results.append(d1.__repr__() == '$2.00')
-        #Check += float
-        d1 += 2.0
-        results.append(d1.val == 4.0)
-        #Check -= float
-        d1 -= 2.0
-        results.append(d1.val == 2.0)
-        #Check +  float
-        #Check -  float
-        #Check >= float
-        results.append(d2 >= 2.0 == True)
-        #Check <= float
-        results.append(2.0 <= d2 == True)
-        #Check >  float
-        results.append(d2 > 2.0 == True)
-        #Check <  float
-        results.append(2.0 < d2 == True)
-        #Check += int
-        d1 += 2
-        results.append(d1.val == 4.0)
-        #Check -= int
-        d1 -= 2
-        results.append(d1.val == 2.0)
-        #Check += USDollar
-        d1 += d2
-        results.append(d1.val == 102.0)
-        #Check -= USDollar
-        d1 -= d2
-        results.append(d1.val == 2.0)
-        self.printResults(results,'uSDTests')
-
-    def runTests(self):
-        self.filterTests()
-        self.matchBracketTests()
-        self.wordCountsTests()
-        self.nthPrimeTests()
-        self.uSDTests()
-        print "{}/{} => {}".format(self.score,self.total,100.0*float(self.score/self.total))
-
-#t = Tester()
-#t.runTests()
